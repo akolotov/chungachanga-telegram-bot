ec5b70764fddce3141f37ba98b104ebe7c52c628
Alexander Kolotov <alexandr.kolotov@gmail.com>
2025-02-10 17:37:33 -0600

    improved version of parsing articles

--- bot/web_parsers/crhoy.py
+++ bot/web_parsers/crhoy.py
@@ -34,46 +34,60 @@ def parse_article(url: str, headers: dict) -> Tuple[str, str]:
         
         soup = BeautifulSoup(content, 'html.parser')
         
-        # Extract title
-        title = soup.find('h1', class_='titulo')
-        title_text = title.text.strip() if title else ""
+        # Determine if this is an opinion piece
+        main_section = soup.find('section', class_='main-content')
+        is_opinion = main_section and 'opinion' in ' '.join(main_section.get('class', []))
         
-        # Extract main content
-        content_div = soup.find('div', {'id': 'contenido'})
-        if content_div:
-            # Get main content div (usually the first div after optional bullet points)
-            main_content = content_div.find('div', recursive=False)
-            if main_content:
-                # Remove unwanted elements
-                unwanted_elements = [
-                    'script', 'style', 'iframe',  # Technical elements
-                    'div.banner-d', 'div.bannerEmbedsHome', 'div.moreBox',  # Ads and recommendations
-                    'div.comentarios-container', 'div.etiquetas',  # Comments and tags
-                    'div.leerMasOuter', 'div.gallery',  # "Read more" links
-                    'div.wp-caption'
-                ]
+        if is_opinion:
+            # Opinion piece format
+            article = main_section.find('article', class_='articulo-opinion')
+            if article:
+                title = article.find('h1')
+                content_div = article.find('div', class_='contenido')
                 
-                for element in unwanted_elements:
-                    for tag in main_content.select(element):
-                        tag.decompose()
-
-                # # Replace closing blockquote tags with a newline after them
-                # main_content_str = str(main_content)
-                # main_content_str = re.sub(r'<blockquote>', '\n<div class="blockquote">\n<blockquote>\n', main_content_str, flags=re.IGNORECASE)
-                # main_content_str = re.sub(r'</blockquote>', '\n</blockquote>\n</div>\n', main_content_str, flags=re.IGNORECASE)
-                # # Remove spaces after opening tags and add space before them
-                # main_content_str = re.sub(r'<([^>]+)>\s+', r' <\1>', main_content_str)
-                # # Remove spaces before closing tags and add space after them
-                # main_content_str = re.sub(r'\s+</([^>]+)>', r'</\1> ', main_content_str)
-
-                # options = pyhtml2md.Options()
-                # options.splitLines = False
-                # converter = pyhtml2md.Converter(main_content_str, options)
-                # content_text = converter.convert()
-                content_text = MarkdownConverter().convert_soup(main_content)
-                
+                title_text = title.text.strip() if title else ""
+                content_text = MarkdownConverter().convert_soup(content_div) if content_div else ""
         else:
-            content_text = ""
+            # Regular article format
+            title = soup.find('h1', class_='titulo')
+            content_div = soup.find('div', {'id': 'contenido'})
+            
+            title_text = title.text.strip() if title else ""
+            
+            if content_div:
+                # Get main content div (usually the first div after optional bullet points)
+                main_content = content_div.find('div', recursive=False)
+                if main_content:
+                    # Remove unwanted elements
+                    unwanted_elements = [
+                        'script', 'style', 'iframe',  # Technical elements
+                        'div.banner-d', 'div.bannerEmbedsHome', 'div.moreBox',  # Ads and recommendations
+                        'div.comentarios-container', 'div.etiquetas',  # Comments and tags
+                        'div.leerMasOuter', 'div.gallery',  # "Read more" links
+                        'div.wp-caption'
+                    ]
+                    
+                    for element in unwanted_elements:
+                        for tag in main_content.select(element):
+                            tag.decompose()
+
+                    # # Replace closing blockquote tags with a newline after them
+                    # main_content_str = str(main_content)
+                    # main_content_str = re.sub(r'<blockquote>', '\n<div class="blockquote">\n<blockquote>\n', main_content_str, flags=re.IGNORECASE)
+                    # main_content_str = re.sub(r'</blockquote>', '\n</blockquote>\n</div>\n', main_content_str, flags=re.IGNORECASE)
+                    # # Remove spaces after opening tags and add space before them
+                    # main_content_str = re.sub(r'<([^>]+)>\s+', r' <\1>', main_content_str)
+                    # # Remove spaces before closing tags and add space after them
+                    # main_content_str = re.sub(r'\s+</([^>]+)>', r'</\1> ', main_content_str)
+
+                    # options = pyhtml2md.Options()
+                    # options.splitLines = False
+                    # converter = pyhtml2md.Converter(main_content_str, options)
+                    # content_text = converter.convert()
+                    content_text = MarkdownConverter().convert_soup(main_content)
+                
+            else:
+                content_text = ""
         
         if not title_text or not content_text:
             raise WebParserError("Failed to extract title or content")
